f0e8418e800729beb08b23961f34077c1387d63e
Udayaditya Singh <udayaditya.singh@gmail.com>
2023-03-12 09:57:47 +0530

    fix

--- frontend/src/components/Transaction.js
+++ frontend/src/components/Transaction.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-
+import ReactSwitch from 'react-switch';
 
 export default function Transaction() {
     const navigate = useNavigate();
     const [amount, setUserAmount] = useState("");
     const [description, setDescription] = useState("");
     const [disableBalance, setBalance] = useState(false);
+    const [checked, setChecked] = useState(true);
+    const [credit, setCredit] = useState('Credit');
+
+    const handleChange = val => {
+      if(val){
+        setCredit('Credit')
+      } else{
+        setCredit('Debit')
+      }
+      setChecked(val)
+    }
     const formHandler = async (ev) => {
         const wallet = JSON.parse(localStorage.getItem("walletId"));
         // console.log("walletttttt",JSON.parse(wallet))
@@ -44,18 +54,19 @@ export default function Transaction() {
                         <hr />
                         <form action="dashboard.html" onSubmit={formHandler}>
                             <div className="form-group">
-                                <input type="number" className="form-control form-control-lg" style={{ margin: '1vw' }} placeholder="amount" name='amount' onChange={ev => { setUserAmount(ev.target.value) }}
+                                <input type="number" className="form-control form-control-lg" style={{ margin: '1vw' }} placeholder="Amount" name='amount' onChange={ev => { setUserAmount(ev.target.value) }}
                                     value={amount} />
                             </div>
                             <div className="form-group">
-                                <input type="text" className="form-control form-control-lg" style={{ margin: '1vw' }} placeholder="description" name='description' onChange={ev => { setDescription(ev.target.value) }}
+                                <input type="text" className="form-control form-control-lg" style={{ margin: '1vw' }} placeholder="Description" name='description' onChange={ev => { setDescription(ev.target.value) }}
                                     value={description} />
                             </div>
                             <div className="toggle-switch">
-                                <input type="checkbox" className="toggle-switch-checkbox-xl" name="toggleSwitch" id="toggleSwitch" />
-                                <label className="toggle-switch-label" for="toggleSwitch">
-                                    Toggle Me!
-                                </label>
+                            <h6>{credit}</h6>
+                            <ReactSwitch
+                                checked={checked}
+                                onChange={handleChange}
+                            />
                             </div>
                             <input type="submit" className="btn btn-primary btn-block mt-4" value="Create" />
                         </form>
